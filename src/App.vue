cf54211f6298838daab4dd3d626a0b425ce037ce
2Gxiaohei <abc2gxiaoh_0125@qq.com>
2025-01-27 14:10:54 +0800

    test2

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 
-//试验接口start
+//这是试验接口start
 import request from './utils/request'
 request({
   method: 'GET', //请求类型
-  url: '/users' //请求地址 实际请求地址要加上之前的baseURL
+  url: '/users', //请求地址 实际请求地址要加上之前的baseURL
   //请求参数（无）
-}).then(res => {
+}).then((res) => {
   console.log(res)
 })
 //试验接口end
@@ -18,5 +18,4 @@ request({
   <RouterView />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
