cbd763927daed8af3af73e2d374c63d3d8f9da2a
2Gxiaohei <abc2gxiaoh_0125@qq.com>
2025-02-07 19:42:41 +0800

    在/about界面完成简单接口实验

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,16 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+
+app.use(ElementPlus);
+
+
+
+
+
+
+
