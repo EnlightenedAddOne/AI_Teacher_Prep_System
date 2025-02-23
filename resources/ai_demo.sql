0f39c467bcb971e7f49618b90d8438fb691ab817
Herion <2357008443@qq.com>
2025-02-23 22:39:06 +0800

    后端第三次提交--数据库文件及配置文件

---
+++ resources/ai_demo.sql
@@ -0,0 +1,12 @@
+-- auto-generated definition
+create table users
+(
+    id       bigint auto_increment
+        primary key,
+    username varchar(255)                         not null,
+    password varchar(255)                         not null,
+    role     enum ('ADMIN', 'TEACHER', 'STUDENT') not null,
+    constraint username
+        unique (username)
+);insert into users (username, password, role) values ('admin', 'admin123', 'ADMIN');
+
